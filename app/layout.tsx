f744facb79e5ba4b8eebce715ec8894fd202850e
MelihKrts <melih.karatas1281@gmail.com>
2025-07-31 22:42:41 +0300

    refactor: clean up unused global styles and switch to localFont in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 import type {Metadata} from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local"
 import "./globals.css";
 import Footer from "@/app/component/layout/Footer";
 import Header from "@/app/component/layout/Header";
-import React from "react";
+import type React from "react";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-//
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const delivery = localFont({
+    src: [
+        {
+            path: "../public/fonts/Delivery_W_Rg.woff2",
+            weight:"normal",
+            style:"normal"
+        },
+    ],
+})
 
 export const metadata: Metadata = {
     title: "Learn TypeScript",
@@ -24,12 +24,10 @@ export default function RootLayout({children,}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="tr">
+        <html lang="tr" className={delivery.className}>
         <link rel="icon" href="/favicon.ico" type="image/svg+xml"/>
 
-        <body
-            // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body>
         <Header/>
         <main className="mt-16 min-h-dvh w-full relative">
             {children}
