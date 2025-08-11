0bc8a8da52852764506c9148bfd0e1d20351060a
MelihKrts <melih.karatas1281@gmail.com>
2025-08-11 19:02:32 +0300

    feat(pwa): add new theme-color properties and screenshots

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,15 +30,17 @@ export default function RootLayout({children,}: Readonly<{
     return (
         <html lang="tr" className={delivery.className}>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
+        <meta name="theme-color" content="#F4E6D8" media="(prefers-color-scheme: light)"/>
+        <meta name="theme-color" content="#5D4037" media="(prefers-color-scheme: dark)"/>
         <link rel="manifest" href="/manifest.json"/>
         <body className="dark:bg-black">
         <ThemeProvider>
-            <Header />
+            <Header/>
             <CheckConnectionControl/>
             <main className="mt-16 min-h-dvh w-full relative">
                 {children}
             </main>
-            <Footer />
+            <Footer/>
         </ThemeProvider>
         </body>
         </html>
