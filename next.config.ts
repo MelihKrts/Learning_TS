70f53d4e9a543c69fe330c2425e185235fa31ab1
MelihKrts <melih.karatas1281@gmail.com>
2025-08-15 18:03:36 +0300

    build(pwa): update next-pwa config and turbopack resolve extensions

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,25 @@
-import withPWA from "next-pwa";
-import runtimeCaching from "next-pwa/cache";
+import withPWA from "next-pwa"
 
 const pwaConfig = withPWA({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    cacheStartUrl: true,
-    reloadOnOnline: true,
-    disable: process.env.NODE_ENV === "development",
-    buildExcludes: [/app-build-manifest.json$/],
-    fallbacks: {
-        document: "/offline", // Next.js page fallback
-    },
-    runtimeCaching, // JS/CSS/HTML/asset caching
-});
+    dest:"public",
+    register:true,
+    skipWaiting:true,
+    disable: process.env.NODE_ENV === 'development',
+    buildExcludes: [/app-build-manifest.json$/]
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     compress: true,
+    reactStrictMode: true,
     compiler: {
-        removeConsole: process.env.NODE_ENV === "production",
+        removeConsole: process.env.NODE_ENV === 'production',
     },
-};
+    turbopack: {
+        resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.json'],
+    }
+}
 
-export default pwaConfig(nextConfig);
+
+export default pwaConfig(nextConfig)
