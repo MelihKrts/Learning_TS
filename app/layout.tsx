d42234d90dd2408a0edb56c1e8755bea6b5a14ab
MelihKrts <melih.karatas1281@gmail.com>
2025-08-18 18:39:22 +0300

    feat: add iOS splash screens for PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,87 @@ export default function RootLayout({children,}: Readonly<{
     return (
         <html lang="tr" className={delivery.className}>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
+        <link rel="apple-touch-icon" href="/ios/128.png"/>
+        {/* iPhone 5, SE (1st gen) – 4 inch */}
+        <link
+            rel="apple-touch-startup-image"
+            href="/ios-splash-img/ip5-ipSE4.jpg"
+            media="(device-width: 320px) and (device-height: 568px)
+  and (-webkit-device-pixel-ratio: 2)
+  and (orientation: portrait)"
+        />
+
+        {/* iPhone 6, 7, 8 */}
+        <link
+            rel="apple-touch-startup-image"
+            href="/ios-splash-img/ip6.jpg"
+            media="(device-width: 375px) and (device-height: 667px)
+  and (-webkit-device-pixel-ratio: 2)
+  and (orientation: portrait)"
+        />
+
+        {/* iPhone X, XS, 11 Pro */}
+        <link
+            rel="apple-touch-startup-image"
+            href="/ios-splash-img/ip11XS.jpg"
+            media="(device-width: 375px) and (device-height: 812px)
+  and (-webkit-device-pixel-ratio: 3)
+  and (orientation: portrait)"
+        />
+
+        {/* iPhone XR, iPhone 11 */}
+        <link
+            rel="apple-touch-startup-image"
+            href="/ios-splash-img/ipXR.jpg"
+            media="(device-width: 414px) and (device-height: 896px)
+  and (-webkit-device-pixel-ratio: 2)
+  and (orientation: portrait)"
+        />
+
+        {/* iPhone XS Max, 11 Pro Max */}
+        <link
+            rel="apple-touch-startup-image"
+            href="/ios-splash-img/ipX-XII.jpg"
+            media="(device-width: 414px) and (device-height: 896px)
+  and (-webkit-device-pixel-ratio: 3)
+  and (orientation: portrait)"
+        />
+
+        {/* iPhone 12, 13, 14, 16 (standart modeller) */}
+        <link
+            rel="apple-touch-startup-image"
+            href="/ios-splash-img/ipXII-XIV.jpg"
+            media="(device-width: 390px) and (device-height: 844px)
+  and (-webkit-device-pixel-ratio: 3)
+  and (orientation: portrait)"
+        />
+
+        {/* iPhone 14 Plus, 14 Pro Max, 15 Pro Max, 16 Plus */}
+        <link
+            rel="apple-touch-startup-image"
+            href="/ios-splash-img/ipXIV-XVI.jpg"
+            media="(device-width: 428px) and (device-height: 926px)
+  and (-webkit-device-pixel-ratio: 3)
+  and (orientation: portrait)"
+        />
+
+        {/* iPhone 14 Pro, 15 Pro, 16 Pro */}
+        <link
+            rel="apple-touch-startup-image"
+            href="/ios-splash-img/ipXIV-XVIpro.jpg"
+            media="(device-width: 430px) and (device-height: 932px)
+  and (-webkit-device-pixel-ratio: 3)
+  and (orientation: portrait)"
+        />
+
+        {/* iPhone 16e */}
+        <link
+            rel="apple-touch-startup-image"
+            href="/ios-splash-img/ip16e.jpg"
+            media="(device-width: 430px) and (device-height: 932px)
+  and (-webkit-device-pixel-ratio: 2)
+  and (orientation: portrait)"
+        />
         <meta name="theme-color" content="#F4E6D8" media="(prefers-color-scheme: light)"/>
         <meta name="theme-color" content="#5D4037" media="(prefers-color-scheme: dark)"/>
         <link rel="manifest" href="/manifest.json"/>
