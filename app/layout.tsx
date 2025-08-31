30f1db365fd28cc3b6cfebca576efa438f34fc49
MelihKrts <melih.karatas1281@gmail.com>
2025-08-31 19:26:56 +0300

    ios PWA splash screen edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,46 +37,47 @@ export default function RootLayout({children,}: Readonly<{
               media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"/>
 
         {/* iPhone 6, 7, 8 */}
-        <link rel="apple-touch-startup-image" href="/ios-splash-img/ip6.png" media="screen(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)
+        <link rel="apple-touch-startup-image" href="/ios-splash-img/ip6.png" media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)
   and (orientation: portrait)"/>
 
         {/* iPhone X, XS, 11 Pro */}
         <link
             rel="apple-touch-startup-image" href="/ios-splash-img/ip11XS.png"
-            media="screen (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"/>
+            media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"/>
 
         {/* iPhone XR, iPhone 11 */}
         <link rel="apple-touch-startup-image" href="/ios-splash-img/ipXR.png"
-              media="screen (device-width: 414px) and (device-height: 896px) and(-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+              media="screen and (device-width: 414px) and (device-height: 896px) and(-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         />
 
         {/* iPhone XS Max, 11 Pro Max */}
         <link rel="apple-touch-startup-image" href="/ios-splash-img/ipX-XII.png"
-              media="screen (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+              media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         />
 
         {/* iPhone 12, 13, 14, 16 (standart modeller) */}
         <link rel="apple-touch-startup-image" href="/ios-splash-img/ipXII-XIV.png"
-              media="screen (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+              media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         />
 
         {/* iPhone 14 Plus, 14 Pro Max, 15 Pro Max, 16 Plus */}
         <link rel="apple-touch-startup-image" href="/ios-splash-img/ipXIV-XVI.png"
-              media="screen (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+              media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         />
 
         {/* iPhone 14 Pro, 15 Pro, 16 Pro */}
         <link rel="apple-touch-startup-image" href="/ios-splash-img/ipXIV-XVIpro.png"
-              media="screen (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+              media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         />
 
         {/* iPhone 16e */}
         <link rel="apple-touch-startup-image" href="/ios-splash-img/ip16e.png"
-              media="screen (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+              media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         />
         <meta name="theme-color" content="#F4E6D8" media="(prefers-color-scheme: light)"/>
         <meta name="theme-color" content="#5D4037" media="(prefers-color-scheme: dark)"/>
         <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-touch-fullscreen" content="yes"/>
         <meta name="mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-title" content="Learn TS"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="default"/>
